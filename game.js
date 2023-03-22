bcd5fe15008966ce621c38cf914bf9adf0082f10
sachin <sachinsain21082004@gmail.com>
2023-03-22 14:47:28 +0530

    22-03-23

--- game.js
+++ game.js
@@ -11,45 +11,62 @@ start.onclick = (e) => {
 let selectimage = "";
 let images = document.querySelectorAll('.wapper2 img')
 // console.log(images)
+let time = 0;
 for (let i = 0; i < images.length; i++) {
     images[i].onclick = () => {
         selectimage = images[i].src;
         wapper2.style.display = "none";
         wapper3.style.display = "flex";
-        letsplay();
+        // letsplay();
+        let x = setInterval(() => {
+            if (time === 30) {
+                clearInterval(x)
+                alert(`${'time up'} 
+                    ${"score:-"} ${score}`)
+            }
+            else {
+                timer.innerHTML = ++time;
+                // repeatimage();
+            }
+        }, 1000);
+        let y = setInterval(() => {
+            repeatimage()
+        }, 500);
 
     }
 }
-const timer=document.querySelector('.timer span')
-const score=document.querySelector('.score span');
- 
-function letsplay(){
-    let time=0;
-    let x=setInterval(() => {
-        if(time===30){
-            clearInterval(x)
-            alert(`${'time up'} 
-                ${"score:-"} ${score}`)
-        }
-        else{
-            timer.innerHTML=++time;
-            repeatimage();
-        }
-    }, 100);
-}
-function repeatimage(){
+const timer = document.querySelector('.timer span')
+const score = document.querySelector('.score span');
 
-    const img=document.querySelector('img');
-    img.setAttribute('src',selectimage);
+function letsplay() {
+
+
+}
+function repeatimage() {
+
+    const img = document.querySelector('img');
+    img.setAttribute('src', selectimage);
 
     // console.log(img)
 
-    img.style.left=randomimgleft();
-    img.style.top=randomimgright();
+    img.style.left = randomimgleft();
+    img.style.top = randomimgtop();
+    img.style.height = "25px";
+    img.style.position = "absolute"
+    img.style.width = "20px"
 
-    img.setAttribute("onclick","removeimg()")
+    img.setAttribute("onclick", "removeimg(this)")
 
-    document.querySelector('.wapper3 .gamearea').innerHTML.append(img);
+    document.querySelector('.wapper3 .gamearea').append(img);
 
 }
+function randomimgleft() {
+    return Math.random() * window.innerWidth + "px";
+}
 
+function randomimgtop() {
+    return Math.random() * window.innerHeight + "px";
+}
+function removeimg(element) {
+    element.remove()
+}
